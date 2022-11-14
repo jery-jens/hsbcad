8260cc125039cec35adc73a39d74ed13b2363bac
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-11-14 12:05:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,27 @@ $(document).ready(function () {
 
   // CASES
 
+      // Options for the observer (which mutations to observe)
+      var config = { attributes: true, childList: true };
+
+      // Callback function to execute when mutations are observed
+      var callback = function(mutationsList) {
+          for(var mutation of mutationsList) {
+              if (mutation.type == 'childList') {
+                  console.log('A child node has been added or removed.');
+              }
+              else if (mutation.type == 'attributes') {
+                  console.log('The ' + mutation.attributeName + ' attribute was modified.');
+              }
+          }
+      };
+  
+      // Create an observer instance linked to the callback function
+      var observer = new MutationObserver(callback);
+  
+      // Start observing the target node for configured mutations
+      observer.observe(document.querySelector(".collection-list"), config);
+
   document.querySelectorAll(".pagination_button").forEach((item, index) => {
     item.addEventListener("click", () => {
         console.log(document.querySelectorAll(".case-tile").length);
